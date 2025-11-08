8a87b5c7688555639e9cd99b4ec4e9dc823fe14d
AMRENDRA KUMAR <AMRENDRAKSTAR@GMAIL.COM>
2025-11-08 04:55:24 +0000

    abc

---
+++ src/App.js
@@ -0,0 +1,8 @@
+function App() {
+    return (
+        <div class='container bg-primary text-white '>
+            <h3>Welcome to My React App!</h3>
+        </div>
+    );
+}
+ReactDOM.render(<App />, document.getElementById('root'));
